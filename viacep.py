099a64b571319219a97d9442c3fe42e0c917ca1d
unknown <wesleyfreire1707@gmail.com>
2024-11-10 19:30:30 -0300

    Função para buscar o cep

--- viacep.py
+++ viacep.py
@@ -1,1 +1,8 @@
 import requests
+
+viacep = "https://viacep.com.br/ws/{cep}/json/"
+
+
+def pega_endereco(cep: str) -> dict:
+    """busca o endereço atraves do cep"""
+    response = requests.get(viacep.replace("{cep}", cep))
