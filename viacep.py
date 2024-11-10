9fc2dae63794ffefb1f18a532c45afb7f643e92a
unknown <wesleyfreire1707@gmail.com>
2024-11-10 19:41:00 -0300

    Projeto com funcionamento ok

--- viacep.py
+++ viacep.py
@@ -6,3 +6,8 @@ viacep = "https://viacep.com.br/ws/{cep}/json/"
 def pega_endereco(cep: str) -> dict:
     """busca o endereço atraves do cep"""
     response = requests.get(viacep.replace("{cep}", cep))
+    if not response.status_code == 200:
+        return {}
+    return response.json()
+
+print(pega_endereco("01001000"))
